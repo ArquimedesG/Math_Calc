7d8ec87fa6c720f203d01d5ff00e3323e2caaa6b
Godoy <argodoy@borgwarner.com>
2025-05-13 19:25:26 -0600

    Multiplication and Division Added to the Calculator

--- calculator.js
+++ calculator.js
@@ -4,3 +4,12 @@ function sumar (a,b) {
 function restar (a,b) {
     return a - b;
 }
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  if (b === 0) throw new Error("Division by zero not allowed");
+  return a / b;
+}
